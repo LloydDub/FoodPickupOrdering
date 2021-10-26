e5c037933b47581e230df2521923bd6e2ec0c3fe
marcschul <marcschulm@gmail.com>
2021-10-26 16:51:01 +0000

    change password string in db to 'password' for all users

--- db/seeds/02_customers.sql
+++ db/seeds/02_customers.sql
@@ -1,7 +1,6 @@
 INSERT INTO customers (name, email, password, phone, address_id) VALUES
 ('admin', 'admin@email.com', 'password', '12459863658', 1),
-('Marcis Awzum', 'mschul@hotmail.com', 'upthestairs', '18264409800', 3),
-('Doug Slacken', 'dboy@hotmail.ca', 'outthedoor', '18390072564', 2),
-('Felicia Bye', 'byefelicia@gmail.com', 'overthefence', '12895467341', 1),
-('Jutch Jeudie', 'lawislife@gmail.ca', 'downtheslide', '19208978878', 1),
-('admin', 'admin@email.com', 'admin', '00000000000', 3);
+('Marcis Awzum', 'mschul@hotmail.com', 'password', '18264409800', 3),
+('Doug Slacken', 'dboy@hotmail.ca', 'password', '18390072564', 2),
+('Felicia Bye', 'byefelicia@gmail.com', 'password', '12895467341', 1),
+('Jutch Jeudie', 'lawislife@gmail.ca', 'password', '19208978878', 1)
