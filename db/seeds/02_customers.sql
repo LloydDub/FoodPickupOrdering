6ae7b3d75831da35abe39ed1a008327509acc337
marcschul <marcschulm@gmail.com>
2021-10-26 05:21:08 +0000

    add admin to customer db

--- db/seeds/02_customers.sql
+++ db/seeds/02_customers.sql
@@ -1,4 +1,5 @@
-INSERT INTO customers (name, email, password, phone, address_id) VALUES ('Lloyd Isdaman', 'lloydguy@gmail.com', 'intheweeds', '12459863658', 4),
+INSERT INTO customers (name, email, password, phone, address_id) VALUES
+('admin', 'admin@email.com', 'password', '12459863658', 1),
 ('Marcis Awzum', 'mschul@hotmail.com', 'upthestairs', '18264409800', 3),
 ('Doug Slacken', 'dboy@hotmail.ca', 'outthedoor', '18390072564', 2),
 ('Felicia Bye', 'byefelicia@gmail.com', 'overthefence', '12895467341', 1),
