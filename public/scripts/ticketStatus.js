a5872ba5ca15e132dc30bbf1b1c7743a1081e45d
Lloyd Worth <lloydrww@gmail.com>
2021-10-28 18:23:09 +0000

    working on order status

--- public/scripts/ticketStatus.js
+++ public/scripts/ticketStatus.js
@@ -3,7 +3,23 @@ $(() => {
     method: "GET",
     url: "/api/tickets",
   }).done(function (tickets) {
-    console.log("!!!!", tickets.tickets.ticket_num);
-    $("#order").append(`<p>Ticket Num: ${tickets.tickets.ticket_num}</p>`);
+    console.log("!!!!", tickets.tickets);
+    $("#order").append(`<p>Ticket Num: ${tickets.tickets.ticket_num}</p>
+
+    <p> Order Created: ${tickets.tickets.created_at}</p>
+    <p>Order Completed: ${tickets.tickets.finished_at}</p>
+    `);
+  });
+
+  $.ajax({
+    method: "GET",
+    url: "/api/customers",
+  }).done(function (response) {
+    for (customer of response.customers) {
+      console.log("AAAA!", customer);
+
+      $("#order").append(`<p>Customer Name: ${customer.name}</p>
+      Tel: ${customer.phone}</p>`);
+    }
   });
 });
--- routes/customers.js
+++ routes/customers.js
@@ -1,69 +1,60 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = function(db) {
-
+module.exports = function (db) {
   /**
-  * Endpoint ==> GET /customers
-  * localhost:8080/api/customers/
-  * returns an JSON object of all customers with their information
-  */
-  router.get("/", function(req, res) {
-    db
-      .query(`SELECT * FROM customers`)
-      .then(data => {
+   * Endpoint ==> GET /customers
+   * localhost:8080/api/customers/
+   * returns an JSON object of all customers with their information
+   */
+  router.get("/", function (req, res) {
+    db.query(`SELECT * FROM customers`)
+      .then((data) => {
         const customers = data.rows;
         res.json({ customers });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   /**
-  * Endpoint ==> POST /customers
-  * localhost:8080/api/customers/
-  * handles a request to add an individual customer to db
-  */
-  router.post("/", function(req, res) {
+   * Endpoint ==> POST /customers
+   * localhost:8080/api/customers/
+   * handles a request to add an individual customer to db
+   */
+  router.post("/", function (req, res) {
     // const inputName = req.body.name;
-    const params = [inputName]
+    const params = [inputName];
     const query = `INSERT INTO customers (name) VALUES ($1)`;
 
-    db
-      .query(query, params)
-      .then(res.redirect('/'))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+    db.query(query, params)
+      .then(res.redirect("/"))
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
   /**
-  * Endpoint ==> GET /customers/:id
-  * localhost:8080/api/customers/:id
-  * returns a specific customer given their customer id
-  */
-    router.get("/:id", function(req, res) {
-      let customerId = req.params.id;
-      const params = [customerId];
-      const query = `SELECT * FROM customers WHERE id = $1`;
+   * Endpoint ==> GET /customers/:id
+   * localhost:8080/api/customers/:id
+   * returns a specific customer given their customer id
+   */
+  router.get("/:id", function (req, res) {
+    let customerId = req.params.id;
+    const params = [customerId];
+    const query = `SELECT * FROM customers WHERE id = $1`;
 
-      db
-        .query(query, params)
-        .then(data => {
-          const customers = data.rows;
-          res.json({ customers });
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
+    db.query(query, params)
+      .then((data) => {
+        const customers = data.rows;
+        res.json({ customers });
+        console.log("cvccv", data.rows);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-return router;
-}
+  return router;
+};
